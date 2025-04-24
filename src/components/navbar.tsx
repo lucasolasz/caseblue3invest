46e9ef25c4d753607ced84cf3dd82a2c57fd255c
conuaz <lucas.olasz@tty2000.com.br>
2025-04-24 18:45:10 -0300

    Aumenta a altura da nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 export function Navbar() {
   return (
-    <div className="flex items-center justify-betweem p-4 text-white">
+    <div className="flex items-center justify-betweem p-4 text-white h-24">
       <h1>Navbar</h1>
     </div>
   );
