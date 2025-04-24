c7a1808f76c448ca36173d6f701368f04a5211ac
conuaz <lucas.olasz@tty2000.com.br>
2025-04-24 18:42:48 -0300

    hero pronto mobile

---
+++ src/components/btnAbraSuaConta.tsx
@@ -0,0 +1,7 @@
+export default function BtnAbraSuaConta() {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-900 px-4 py-3 text-white rounded cursor-pointer my-6">
+      Abra sua conta
+    </button>
+  );
+}
