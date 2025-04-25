0349a9e5ea514eeacfb162fc4abb7e9532305c22
conuaz <lucas.olasz@tty2000.com.br>
2025-04-25 19:02:44 -0300

    Mudança na estrutura do projeto

---
+++ src/app/corporate/hero.tsx
@@ -0,0 +1,31 @@
+import BtnAbraSuaConta from "@/components/btnAbraSuaConta";
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div className="h-screen rounded-br-4xl bg-linear-to-t from-slate-800 to-blue-950">
+      <div className="flex flex-col items-center justify-center">
+        <h3 className="text-blue-500 pt-12 ">PARA SUA EMPRESA</h3>
+
+        <h1 className="text-4xl font-bold text-white text-center p-10 ">
+          O suporte que a sua organização precisa para crescer cada vez mais
+        </h1>
+        <p className="text-blue-500 font-medium">De capital de giro ao IPO</p>
+        <p className="text-center text-white px-4">
+          Conte com um time de assessores, bankers e analistas especializados
+          para te ajudar a escalar o seu negócio e levá-lo para o próximo
+          patamar.
+        </p>
+        <BtnAbraSuaConta></BtnAbraSuaConta>
+
+        <Image
+          className="mx-auto my-4 bg-transparent"
+          src="/assets/corporate-header-mobile.png"
+          alt="Analytics"
+          width={298}
+          height={100}
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,31 +1,32 @@
 import BtnAbraSuaConta from "./btnAbraSuaConta";
 import Image from "next/image";
 
-export function Hero() {
+export default function Hero() {
   return (
-    <div className="h-screen rounded-br-4xl bg-linear-to-t from-slate-800 to-blue-950">
-      <div className="flex flex-col items-center justify-center">
-        <h3 className="text-blue-500 pt-12 ">PARA SUA EMPRESA</h3>
-
-        <h1 className="text-4xl font-bold text-white text-center p-10 ">
-          O suporte que a sua organização precisa para crescer cada vez mais
-        </h1>
-        <p className="text-blue-500 font-medium">De capital de giro ao IPO</p>
-        <p className="text-center text-white px-4">
-          Conte com um time de assessores, bankers e analistas especializados
-          para te ajudar a escalar o seu negócio e levá-lo para o próximo
-          patamar.
-        </p>
-        <BtnAbraSuaConta></BtnAbraSuaConta>
-
-        <Image
-          className="mx-auto my-4 bg-transparent"
-          src="/assets/corporate-header-mobile.png"
-          alt="Analytics"
-          width={298}
-          height={100}
-        />
-      </div>
+    <div className="h-screen bg-[#000d38] rounded-br-4xl text-center p-6">
+      <h1 className="text-white text-4xl font-extrabold pt-12">
+        Invista na sua{" "}
+      </h1>
+      <h1 className="text-blue-500 text-4xl font-extrabold">jornada</h1>
+      <p className="text-white px-12">
+        Seja qual for o seu momento de vida,{" "}
+        <b>estaremos lado a lado para te guiar</b> na sua jornada de
+        investimentos.
+      </p>
+      <BtnAbraSuaConta></BtnAbraSuaConta>
+      <Image
+        src="/assets/logo-banner-1.png"
+        width={450}
+        height={200}
+        alt={""}
+      ></Image>
+      <Image
+        className="mx-auto my-4 bg-transparent"
+        src="/assets/header-home-lg.png"
+        alt="Analytics"
+        width={450}
+        height={200}
+      ></Image>
     </div>
   );
 }
