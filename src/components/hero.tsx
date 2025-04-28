efbf90f90f819e7d971f746acc636fe96eddc605
lucasolasz <olaszlucas072@gmail.com>
2025-04-27 23:36:06 -0300

    Primeiros ajustes para ficar em forma de grid

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,30 +3,28 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="h-screen bg-[#000d38] rounded-br-4xl text-center p-6">
-      <h1 className="text-white text-4xl font-extrabold pt-12">
-        Invista na sua{" "}
-      </h1>
-      <h1 className="text-blue-500 text-4xl font-extrabold">jornada</h1>
-      <p className="text-white px-12">
-        Seja qual for o seu momento de vida,{" "}
-        <b>estaremos lado a lado para te guiar</b> na sua jornada de
-        investimentos.
-      </p>
-      <BtnAbraSuaConta></BtnAbraSuaConta>
-      <Image
-        src="/assets/logo-banner-1.png"
-        width={450}
-        height={200}
-        alt={""}
-      ></Image>
-      <Image
-        className="mx-auto my-4 bg-transparent"
-        src="/assets/header-home-lg.png"
-        alt="Analytics"
-        width={450}
-        height={200}
-      ></Image>
+    <div className="relative h-[90vh] bg-[#000d38] rounded-br-4xl text-center overflow-hidden">
+      <div className="grid md:grid-cols-2 h-full">
+        <div className="flex flex-col justify-center m-20">
+          <h1 className="text-white text-4xl font-extrabold pt-12 ">
+            Invista na sua{" "}
+          </h1>
+          <h1 className="text-blue-500 text-4xl font-extrabold">jornada</h1>
+          <p className="text-white px-12">
+            Seja qual for o seu momento de vida,{" "}
+            <b>estaremos lado a lado para te guiar</b> na sua jornada de
+            investimentos.
+          </p>
+          <BtnAbraSuaConta></BtnAbraSuaConta>
+        </div>
+        <div className="md:flex md:flex-col md:justify-center">
+          <img
+            className="absolute h-[90vh] w-[90vh]"
+            src="/assets/fundo-linhas-azul-home.png"
+            alt="Header Home"
+          />
+        </div>
+      </div>
     </div>
   );
 }
