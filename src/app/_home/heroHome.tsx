3241bcda5d247b7e4d968c5771accad8e52755aa
conuaz <lucas.olasz@tty2000.com.br>
2025-04-30 19:30:23 -0300

    Div alinhada para a direita

--- src/app/_home/heroHome.tsx
+++ src/app/_home/heroHome.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function HeroHome() {
   return (
-    <div className="h-[75vh] w-full bg-[#000d38] rounded-br-4xl overflow-hidden">
-      <div className="h-full flex">
-        <div className="flex flex-col justify-center md:pr-10 md:pl-90 md:max-w-3xl ">
+    <div className="h-[75vh] w-full bg-[#000d38] rounded-br-4xl overflow-hidden flex justify-end">
+      <div className="flex items-center gap-5">
+        <div className="md:max-w-[400px]">
           <h1 className="text-white text-4xl md:text-6xl font-extrabold">
             Invista na sua <span className="text-blue-500">jornada</span>
           </h1>
@@ -16,23 +16,13 @@ export default function HeroHome() {
           </p>
           <BtnAbraSuaConta isTransparent={true} />
         </div>
-        {/* coluna 2 */}
-        <div className="relative w-full h-full">
-          {/* fundo de linhas azuis */}
+        <div className="h-full md:max-w-[1240px]">
           <Image
             src="/assets/fundo-linhas-azul-home.png"
-            alt="Linhas no fundo"
-            fill
-            className="object-cover bg-transparent"
-            priority
-          />
-          {/* imagem da mulher por cima */}
-          <Image
-            src="/assets/header-home-lg.png"
             alt="Womam Home"
-            fill
-            className="object-contain z-10"
-            priority
+            width={1000}
+            height={1000}
+            className="w-full h-full z-2"
           />
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Teste from "@/components/teste";
 import HomePage from "./_home/homePage";
+import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <HomePage />
     </div>
   );
