292d3ccd5da0305d54a052dff63d0f405c7a15b8
lucasolasz <olaszlucas072@gmail.com>
2025-04-30 01:07:56 -0300

    Permite deixar botao transparente

--- src/components/btnAbraSuaConta.tsx
+++ src/components/btnAbraSuaConta.tsx
@@ -1,6 +1,16 @@
-export default function BtnAbraSuaConta() {
+export default function BtnAbraSuaConta({
+  isTransparent,
+}: {
+  isTransparent?: boolean;
+}) {
   return (
-    <button className="bg-blue-500 hover:bg-blue-900 px-10 py-3 text-white rounded cursor-pointer">
+    <button
+      className={`bg-blue-500 px-10 py-3 rounded cursor-pointer ${
+        isTransparent
+          ? "bg-transparent border-blue-500 border-2 font-bold text-blue-500 hover:bg-blue-950"
+          : "text-white hover:bg-blue-900"
+      }`}
+    >
       Abra sua conta
     </button>
   );
