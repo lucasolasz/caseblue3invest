011e88759948acc9112689c3f11ef791825a71dd
lucasolasz <olaszlucas072@gmail.com>
2025-04-30 00:53:30 -0300

    Remove a margin do botao

--- src/components/btnAbraSuaConta.tsx
+++ src/components/btnAbraSuaConta.tsx
@@ -1,6 +1,6 @@
 export default function BtnAbraSuaConta() {
   return (
-    <button className="bg-blue-500 hover:bg-blue-900 px-10 py-3 text-white rounded cursor-pointer my-6">
+    <button className="bg-blue-500 hover:bg-blue-900 px-10 py-3 text-white rounded cursor-pointer">
       Abra sua conta
     </button>
   );
