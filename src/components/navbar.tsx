1c80e37c825eeb9f7fb9e1ca9b042e15a222fe40
conuaz <lucas.olasz@tty2000.com.br>
2025-05-05 17:53:32 -0300

    Submenu Para você

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,37 +4,61 @@ import { IoIosArrowDown } from "react-icons/io";
 
 export function Navbar() {
   return (
-    <div className="flex items-center px-4 h-22 ">
-      <div className="flex items-center">
-        <Image
-          src="/assets/logo-home.png"
-          alt="Logo"
-          width={200}
-          height={200}
-          className="p-4"
-        />
-        <ul className="flex gap-8 ml-25 text-blue-900">
-          <li className="flex gap-2">
-            Para você{" "}
-            <IoIosArrowDown
-              size={20}
-              className="cursor-pointer text-blue-500"
-            />
-          </li>
-          <li className="mx-10">Para sua empresa </li>
-          <li className="flex gap-2">
-            A Blue3{" "}
-            <IoIosArrowDown
-              size={20}
-              className="cursor-pointer text-blue-500"
-            />
-          </li>
-        </ul>
+    <div className="bg-white w-full group">
+      <div className="flex items-center px-4 h-22 ">
+        <div className="md:flex md:items-center">
+          <Image
+            src="/assets/logo-home.png"
+            alt="Logo"
+            width={200}
+            height={200}
+            className="md:p-4"
+          />
+          <ul className="md:flex md:gap-8 md:ml-25 text-blue-900">
+            <li className="md:flex md:gap-2 ">
+              Para você{" "}
+              <IoIosArrowDown
+                size={20}
+                className="cursor-pointer text-blue-500"
+              />
+            </li>
+            <li className="md:mx-10">Para sua empresa </li>
+            <li className="md:flex md:gap-2">
+              A Blue3{" "}
+              <IoIosArrowDown
+                size={20}
+                className="cursor-pointer text-blue-500"
+              />
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex gap-4 ml-auto ">
+          <BtnSite isTransparent={true}> Fale Conosco</BtnSite>
+          <BtnSite isTransparent={false}> Abra sua conta</BtnSite>
+        </div>
       </div>
 
-      <div className="flex gap-4 ml-auto">
-        <BtnSite isTransparent={true}> Fale Conosco</BtnSite>
-        <BtnSite isTransparent={false}> Abra sua conta</BtnSite>
+      <div className="z-2 hidden md:group-hover:block bg-white w-full absolute md:h-[150px] border-t border-gray-200">
+        <div className="flex md:text-start md:mx-80 md:my-10">
+          <div className="max-w-80">
+            <h1 className="text-blue-500 md:text-1xl">Para a sua jornada</h1>
+            <p className="md:text-[14px] text-gray-500">
+              Conheça todas as soluções da Blue3 para a construção e evolução da
+              sua jornada.
+            </p>
+          </div>
+
+          <div className="md:flex md:flex-col md:mx-30 md:gap-5 w-fit">
+            <a href="#">Renda Variável</a>
+            <a href="#">Renda Fixa</a>
+          </div>
+
+          <div className="md:flex md:flex-col md:gap-5 w-fit">
+            <a href="#">Produtos internacionais</a>
+            <a href="#">HUB de Educação</a>
+          </div>
+        </div>
       </div>
     </div>
   );
