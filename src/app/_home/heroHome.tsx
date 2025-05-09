d4c9799b16a7e6bd6d406f54ac493fd9d60b99bf
conuaz <lucas.olasz@tty2000.com.br>
2025-05-09 18:19:08 -0300

    Versão 2.0 do hero home

--- src/app/_home/heroHome.tsx
+++ src/app/_home/heroHome.tsx
@@ -3,24 +3,20 @@ import Image from "next/image";
 
 export default function HeroHome() {
   return (
-    <div className="flex items-center justify-center h-[700px] bg-[#000d38] relative w-screen">
-      {/* Div base centralizada com 700px */}
-      <div className="w-[1140px] h-full relative z-3 flex justify-end">
-        <Image
-          src="/assets/header-home-mulher-mobile.png" // Substitua pelo caminho da nova imagem
-          alt="Mulher frente"
-          width={1000} // Ajuste o tamanho conforme necessário
-          height={1000}
-          className="bg-transparent absolute md:bottom-0 md:h-full md:max-w-[750px]"
-        />
-      </div>
-
-      {/* Duas divs sobrepostas centralizadas (1000px total) */}
-      <div className="absolute h-full flex w-full gap-10 justify-center">
-        <div
-          id="textos"
-          className="ml-90 w-[600px] h-full flex flex-col justify-center"
-        >
+    <div className="md:flex md:justify-center h-[700px] bg-[#000d38] md:relative">
+      {/* Div central */}
+      <div className="md:ml-30 md:h-full md:flex md:justify-center md:gap-10">
+        <div className="md:h-full md:absolute md:right-0">
+          <Image
+            src="/assets/fundo-linhas-azul-home.png"
+            alt="Linhas azuis de fundo"
+            width={1000}
+            height={1000}
+            className="md:w-full md:h-full z-1"
+          />
+        </div>
+        {/* Texto com botão */}
+        <div className="md:max-w-[400px] md:h-full md:flex md:flex-col md:justify-center">
           <h1 className="text-white text-4xl md:text-6xl font-extrabold">
             Invista na sua <span className="text-blue-500">jornada</span>
           </h1>
@@ -31,16 +27,14 @@ export default function HeroHome() {
           </p>
           <BtnSite isTransparent={true}> Abra sua conta</BtnSite>
         </div>
-
-        <div className="w-450 h-full flex items-center justify-end">
-          <Image
-            src="/assets/fundo-linhas-azul-home.png"
-            alt="Linhas azuis de fundo"
-            width={1000}
-            height={1000}
-            className="w-full h-full"
-          />
-        </div>
+        {/* </div> */}
+        <Image
+          src="/assets/header-home-lg.png" // Substitua pelo caminho da nova imagem
+          alt="Mulher frente"
+          width={1000} // Ajuste o tamanho conforme necessário
+          height={1000}
+          className="md:bottom-0 md:h-full md:max-w-[897px] md:z-1"
+        />
       </div>
     </div>
   );
