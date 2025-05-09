8c9d38d0ef7822ae1cabe5d0ebf24b3d8caea7f1
conuaz <lucas.olasz@tty2000.com.br>
2025-05-09 19:25:05 -0300

    Oportunidades , mas falta corrigir o botao e fazer o carrossel

--- src/app/_home/oportunidades.tsx
+++ src/app/_home/oportunidades.tsx
@@ -1,8 +1,13 @@
+import BtnSite from "@/components/btnSite";
+
 export default function Oportunidades() {
   return (
-    <div className="h-[700px] bg-blue-900 flex items-center">
-      <div className="md:max-w-[1140px] mx-auto w-full h-[70%]">
-        Oportunidades
+    <div className="h-[700px] bg-white flex items-center">
+      <div className="md:max-w-[1140px] mx-auto w-full h-[70%] flex flex-col justify-between">
+        <h1 className="md:text-5xl font-extrabold text-blue-500 w-250">
+          Oportunidades de investimentos para o seu perfil
+        </h1>
+        <BtnSite isTransparent={false}>Fale com um acessor Blue3</BtnSite>
       </div>
     </div>
   );
