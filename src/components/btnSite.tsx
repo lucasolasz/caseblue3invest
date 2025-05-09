1f2f1e4bec16a7e2c9b610b26be942f750386e73
conuaz <lucas.olasz@tty2000.com.br>
2025-05-09 16:46:39 -0300

    Ajusta altura do botão.

--- src/components/btnSite.tsx
+++ src/components/btnSite.tsx
@@ -7,7 +7,7 @@ export default function BtnSite({
 }) {
   return (
     <button
-      className={`bg-blue-500 w-[200px] rounded cursor-pointer duration-550 ${
+      className={`bg-blue-500 w-[200px] h-[50px] rounded cursor-pointer duration-550 ${
         isTransparent
           ? "bg-transparent border-blue-500 border-2 font-bold text-blue-500 hover:bg-transparent"
           : "text-white hover:bg-blue-900"
