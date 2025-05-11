2d598762be51a9181611f4eed82f56460832a0bb
lucasolasz <olaszlucas072@gmail.com>
2025-05-11 13:49:11 -0300

    Corrige as cores.

--- src/app/_home/alemDosNumeros.tsx
+++ src/app/_home/alemDosNumeros.tsx
@@ -2,7 +2,7 @@ import { FaPlus } from "react-icons/fa";
 
 export default function AlemDosNumeros() {
   return (
-    <div className="h-[350px] bg-blue-900 flex items-center">
+    <div className="h-[350px] bg-[#000d38] flex items-center">
       <div className="md:max-w-[1140px] mx-auto w-full h-[70%] flex">
         <div className="w-full">
           <div className="max-w-[430px]">
@@ -16,37 +16,39 @@ export default function AlemDosNumeros() {
           </div>
         </div>
         <div className="w-full flex flex-col">
-          <div className="flex h-full">
+          <div className="flex h-full !border-t-[0.5px] !border-b-[0.5px] border-white">
             <div className="flex items-center text-[40px] font-bold mr-4 w-[120px]">
               <span className="mr-2">
-                <FaPlus />
+                <FaPlus className="text-blue-600" />
               </span>
-              37
+              <span className="text-white">37</span>
             </div>
             <div className="flex flex-col justify-center">
               <p className="font-bold text-blue-600">mil</p>
-              <p>que confiam na Blue3</p>
+              <p className="text-white">que confiam na Blue3</p>
             </div>
           </div>
           <div className="flex h-full">
             <div className="flex items-center text-[40px] font-bold mr-4 w-[120px]">
               <span className="mr-2">
-                <FaPlus />
+                <FaPlus className="text-blue-600" />
               </span>
-              28
+              <span className="text-white">28</span>
             </div>
             <div className="flex flex-col justify-center">
               <p className="font-bold text-blue-600">bilhões</p>
-              <p>de reais sob custódia </p>
+              <p className="text-white">de reais sob custódia </p>
             </div>
           </div>
-          <div className="flex h-full">
-            <div className="flex items-center text-[40px] font-bold mr-4 w-[120px]">
+          <div className="flex h-full border-t-[0.5px] border-b-[0.5px] border-white">
+            <div className="flex items-center text-[40px] font-bold mr-4 w-[120px] text-white">
               93,5
             </div>
             <div className="flex flex-col justify-center">
               <p className="font-bold text-blue-600">NPS</p>
-              <p>alto nível de satisfação dos clientes</p>
+              <p className="text-white">
+                alto nível de satisfação dos clientes
+              </p>
             </div>
           </div>
         </div>
